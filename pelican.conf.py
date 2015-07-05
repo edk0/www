c5152d67dfc5766a8e5850bfe62aedca300ff5bc
Ed Kellett <edk141@gmail.com>
2015-07-05 16:51:51 +0000

    update url

--- pelican.conf.py
+++ pelican.conf.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'edk'
 SITENAME = "edk's notes"
-SITEURL = 'http://notes.glin.es'
+SITEURL = 'https://kellett.im'
 TIMEZONE = "Europe/London"
 
 GITHUB_URL = 'http://github.com/edk0/'
